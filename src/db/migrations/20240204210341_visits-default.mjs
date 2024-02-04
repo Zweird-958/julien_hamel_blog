b84b6c899ac45ab6bd297afeaa40efd15ad74454
HAMEL Julien <zweir.dd@gmail.com>
2024-02-04 22:15:50 +0100

    fix: can't run visits-default migration

--- src/db/migrations/20240204210341_visits-default.mjs
+++ src/db/migrations/20240204210341_visits-default.mjs
@@ -1,11 +1,11 @@
 export const up = async (db) => {
   await db.schema.alterTable("posts", (table) => {
-    table.integer("visits").default(0)
+    table.integer("visits").default(0).alter()
   })
 }
 
 export const down = async (db) => {
   await db.schema.alterTable("posts", (table) => {
-    table.integer("visits").default(1)
+    table.integer("visits").default(1).alter()
   })
 }
